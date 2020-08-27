09cc910a1dc0b8e6e49115f341da2f489849eaca
patfitz95 <patfitz95@gmail.com>
2020-08-27 17:32:56 -0500

    app done

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
 
 const Car = (props) => {
+    let car = cars.find(item => props.match.params.id == item.id)
     return (
-        <h1>A specific car</h1>
+        <Container maxWidth="sm">
+            <Paper elevation={5}>
+            <h1>{car.Name}</h1>
+            <Chip label={`Id:${car.id}`} />
+            <Chip label={`Name:${car.Name}`} />
+            <Chip label={`Miles Per Gallon:${car.Miles_per_Gallon}`} />
+            <Chip label={`Cylinders:${car.Cylinders}`} />
+            <Chip label={`Displacement:${car.Displacement}`} />
+            <Chip label={`Horsepower:${car.Horsepower}`} />
+            <Chip label={`Weight in lbs :${car.Weight_in_lbs}`} />
+            <Chip label={`Acceleration:${car.Acceleration}`} />
+            <Chip label={`Year:${car.Year}`} />
+            <Chip label={`Origin:${car.Origin}`} />
+
+            </Paper>
+
+        
+      </Container>
     )
 }
 
