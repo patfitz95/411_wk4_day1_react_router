0d094f86dc271869aded1e3df4744f6cadcbcb79
patfitz95 <patfitz95@gmail.com>
2020-08-27 19:35:45 -0500

    actually done

--- src/components/Car.js
+++ src/components/Car.js
@@ -7,24 +7,16 @@ import Chip from '@material-ui/core/Chip';
 const Car = (props) => {
     let car = cars.find(item => props.match.params.id == item.id)
     return (
-        <Container maxWidth="sm">
-            <Paper elevation={5}>
-            <h1>{car.Name}</h1>
-            <Chip label={`Id:${car.id}`} />
-            <Chip label={`Name:${car.Name}`} />
-            <Chip label={`Miles Per Gallon:${car.Miles_per_Gallon}`} />
-            <Chip label={`Cylinders:${car.Cylinders}`} />
-            <Chip label={`Displacement:${car.Displacement}`} />
-            <Chip label={`Horsepower:${car.Horsepower}`} />
-            <Chip label={`Weight in lbs :${car.Weight_in_lbs}`} />
-            <Chip label={`Acceleration:${car.Acceleration}`} />
-            <Chip label={`Year:${car.Year}`} />
-            <Chip label={`Origin:${car.Origin}`} />
-
+        <Container maxWidth="sm" className="car-container">
+            <Paper className="car-paper">
+                <h2>{car.Name}</h2>
+                {
+                    Object.keys(car).map((key, idx) => {
+                        return <Chip label={`${key}: ${car[key]}`}></Chip>
+                    })
+                }
             </Paper>
-
-        
-      </Container>
+        </Container>
     )
 }
 
